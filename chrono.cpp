e867e1902ee3b6143d4d0afdd2b2f483add9f563
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-19 20:54:14 +0100

    describe method with takt/s

--- chrono.cpp
+++ chrono.cpp
@@ -19,8 +19,10 @@
 using namespace std;
 
 void chronofunct(){
-    chrono::duration<int> sek20(20); // 20s, bo okres(duration) domyślnie taktuje sekundami..
-    chrono::duration<int, ratio<60>> min3; // 3min = || int min =3, a ratio do sekundy(60s ==> 1 minuta)..
+    chrono::duration<int> sek20(20);                 //  20 sekund, bo okres(duration) domyślnie taktuje sekundami..
+    chrono::duration<int, ratio<60>> min3;           //  3  min = || int min =3, a ratio do sekundy(60s ==> 1 minuta)..
+    chrono::duration<double, ratio<10>> x(0.5);      //  5  sekund, bo jest to polowa 10sekundowego taktu (10 x 0.5)..
+    chrono::duration<long, ratio<1,1000>> ms(5);     //  5  milisekund, bo to jest (1 / 1000sekundy)..
 
 }
 
