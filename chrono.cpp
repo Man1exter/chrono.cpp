220c8f8736a845f759ec58d6a5de4fb01bde26ad
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-19 21:17:31 +0100

    see numbers of takt/s

--- chrono.cpp
+++ chrono.cpp
@@ -18,18 +18,36 @@
 
 using namespace std;
 
-void chronofunct(){
+void chronoBasicFunct(){
     chrono::duration<int> sek20(20);                 //  20 sekund, bo okres(duration) domyślnie taktuje sekundami..
     chrono::duration<int, ratio<60>> min3;           //  3  min = || int min =3, a ratio do sekundy(60s ==> 1 minuta)..
     chrono::duration<double, ratio<10>> x(0.5);      //  5  sekund, bo jest to polowa 10sekundowego taktu (10 x 0.5)..
     chrono::duration<long, ratio<1,1000>> ms(5);     //  5  milisekund, bo to jest (1 / 1000sekundy)..
-
+    chrono::seconds sek(15);                         //  15 sekund..
 }
 
+void chronoSomeDay(){
+    chrono::hours day(24);                           // 24  godziny..
+    using namespace chrono;
+    auto okres = minutes(2);
+    cout << " specyficzna nazwa okresu =====> "<< typeid(okres).name() << endl;
 
+    chrono::milliseconds(10);
+    chrono::microseconds(10);
+    chrono::nanoseconds(10);
+}
+
+void chronoCout(){
+    chrono::duration<int> sek(20);   
+    cout << sek.count() << endl;                    // oznacza =====> ile sekund? (zwraca ilość taktów)..
+    sek++;                                          // INKREMENTACJA..
+    cout << sek.count() << endl;                    // oznacza =====> ile sekund? (zwraca ilość taktów)..
+}
 
 int main(){
-    chronofunct();
+    chronoBasicFunct();
+    chronoSomeDay();
+    chronoCout();
     
     return 0;
 }
