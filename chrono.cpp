925462d23b94e23d43b224962b45f85cbb9cee16
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-19 17:50:41 +0100

    basic structure of file with cpp

---
+++ chrono.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+#include <set>
+#include <list>
+#include <forward_list>
+#include <typeinfo>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <typeinfo>
+#include <set>
+#include "numbers.h"
+
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
